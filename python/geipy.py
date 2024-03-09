fb6aa0c7b5008c56805ff9a71f8edcac6c784d99
lancenonce <lancendavis47@gmail.com>
2024-03-09 12:40:28 +0000

    feat: requirements and planning tasks

--- python/geipy.py
+++ python/geipy.py
@@ -1,8 +1,34 @@
+from web3 import Web3
 from pyflipper.pyflipper import PyFlipper
+from giza_actions.action import Action, action
+from giza_actions.agent import GizaAgent
+from giza_actions.task import task
+from dotenv import load_dotenv
+from eth_account import Account
 
-flipper = PyFlipper(com="/dev/cu.usbmodemflip_Anen1x1") 
+load_dotenv()
 
-file_path = "/ext/dageiger.csv"
-csv_data = flipper.storage.read(file=file_path)
+# Import account
+def import_account(mnemonic):
+    account = Account.from_mnemonic(mnemonic)
+    return account
 
-print(csv_data)
+# Read CSV files in `data` folder and process the most recent file reads into a numpy tensor
+
+# Read csv from Flipper
+def read_csv_from_flipper():
+    """
+    Reads the CSV file from the geiger counter values on flipper
+    """
+    flipper = PyFlipper(com="/dev/cu.usbmodemflip_Anen1x1") 
+
+    file_path = "/ext/dageiger.csv"
+    csv_data = flipper.storage.read(file=file_path)
+
+    print(csv_data)
+    
+
+# Address receiver and model launcher
+
+# Action: 
+# Action main function
